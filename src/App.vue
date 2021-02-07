4450e3d238304d76af3a492aa7096e8b5a2571da
= <=>
2021-02-07 22:26:32 +0200

    Make unflip all cards except equals and make some fixes

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div id="app">
-    <div class="counter">
+    <div class="counterBox">
       <h1>
         {{counter}}
       </h1>
+
     </div>
-    <section class="memory-game">
+    <button v-if="finishGame" @click="startGame" class="newButton">Start new game</button>
+    <section class="memory-game" v-else>
       <div v-for="(card, index) in shuffledCards" :key="index" class="memory-card" :class="card.visible ? classes : ''" :data-framework="card.name" @click="toggle(card.id)">
        <img class="front-face" svg-inline :src="card.image" :alt="card.name" />
 
@@ -25,84 +27,94 @@ export default {
       ],
       currentCard: {},
       selectedCards: 0,
+      equalCards: 0,
       cards: [
         {
           'id': '0',
           'name': 'react',
           'image': require('./assets/img/react.svg'),
-          'visible': false
+          'visible': false,
+          'selCard' : false
         },
         {
           'id': '1',
           'name': 'react',
           'image': require('./assets/img/react.svg'),
-          'visible': false
+          'visible': false,
+          'selCard' : false
         },
         {
           'id': '2',
           'name': 'backbone',
           'image': require('./assets/img/backbone.svg'),
-          'visible': false
+          'visible': false,
+          'selCard' : false
         },
         {
           'id': '3',
           'name': 'backbone',
           'image': require('./assets/img/backbone.svg'),
-          'visible': false
+          'visible': false,
+          'selCard' : false
         },
         {
           'id': '4',
           'name': 'aurelia',
           'image': require('./assets/img/aurelia.svg'),
-          'visible': false
+          'visible': false,
+          'selCard' : false
         },
         {
           'id': '5',
           'name': 'aurelia',
           'image': require('./assets/img/aurelia.svg'),
-          'visible': false
+          'visible': false,
+          'selCard' : false
         },
         {
           'id': '6',
           'name': 'vue',
           'image': require('./assets/img/vue.svg'),
-          'visible': false
+          'visible': false,
+          'selCard' : false
         },
         {'id': '7',
           'name': 'vue',
           'image': require('./assets/img/vue.svg'),
-          'visible': false
+          'visible': false,
+          'selCard' : false
         },
         {
           'id': '8',
           'name': 'angular',
           'image': require('./assets/img/angular.svg'),
-          'visible': false
+          'visible': false,
+          'selCard' : false
         },
         {
           'id': '9',
           'name': 'angular',
           'image': require('./assets/img/angular.svg'),
-          'visible': false
+          'visible': false,
+          'selCard' : false
         },
         {
           'id': '10',
           'name': 'ember',
           'image': require('./assets/img/ember.svg'),
-          'visible': false
+          'visible': false,
+          'selCard' : false
         },
         {
           'id': '11',
           'name': 'ember',
           'image': require('./assets/img/ember.svg'),
-          'visible': false
+          'visible': false,
+          'selCard' : false
         },
       ],
-      savedCards: [],
+      finishGame: true
     }
-  },
-  created(){
-    this.timer();
   },
   computed:{
     shuffledCards(){
@@ -115,20 +127,36 @@ export default {
         newValue.name = this.currentCard.name
       }
       if(oldValue.name === newValue.name && this.selectedCards > 1){
-        if(this.selectedCards === 4) {
-          if(confirm('Game finish')){
-            setTimeout(this.gameEnd,1000)
-          }
-
-        }
+        oldValue.selCard = true
+        newValue.selCard = true
+        this.equalCards += 1;
       }else{
         if(this.selectedCards % 2 === 0 && this.selectedCards > 1)
-          setTimeout(this.gameEnd,1000)
+          setTimeout(this.unflipCards,1000)
 
+      }
+    },
+    equalCards(){
+      if(this.equalCards === 6){
+        if(confirm('Game finish!You open all cards!Did you want new game ?')){
+          this.gameEnd()
+        }
+      }
+    },
+    counter(){
+      if(this.counter === 0 ){
+          if(confirm('Game finish!Your time is over!Did you want new game?')){
+            this.gameEnd()
+        }
       }
     }
   },
   methods:{
+    startGame(){
+      this.counter = 60
+      this.timer()
+      this.finishGame = false
+    },
     shuffleArray(array){
       var counter = array.length, temp, index;
 
@@ -150,33 +178,37 @@ export default {
 
       if(this.currentCard.visible){
         this.selectedCards++;
-
       }else{
-
         this.selectedCards--;
       }
 
     },
-
     timer(){
       if(this.counter > 0) {
         setTimeout(() => {
           this.counter -= 1
           this.timer()
         }, 1000)
-      }else{
-        alert('Game finish')
       }
     },
+    unflipCards(){
+      this.shuffledCards.forEach(val => {
+        if(!val.selCard ) {
+          val.visible = false
+        }
+      })
+      this.selectedCards = 0;
+    },
     gameEnd(){
-
       this.shuffledCards.forEach(val => {
         val.visible = false
+        val.selCard = false
       })
       this.currentCard = {}
-      this.selectedCards = 0
-      this.counter = 60
-
+      this.selectedCards = 0;
+      this.equalCards = 0
+      this.counter = 0
+      this.finishGame = true;
     }
   }
 }
@@ -184,4 +216,25 @@ export default {
 
 <style>
 @import './assets/styles.css';
+
+.newButton{
+  display:block;
+  margin:0 auto;
+  background: blue;
+  border:0;
+  padding:15px;
+  color:white;
+  text-transform: UPPERCASE;
+  border-radius:10px;
+  font-weight: bold;
+  font-size:24px;
+  cursor: pointer;
+}
+.counterBox{
+  display: block;
+  margin:0 auto;
+  text-align:center;
+  margin-bottom:30px;
+  color:black;
+}
 </style>
